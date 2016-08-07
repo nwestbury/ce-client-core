d3cf23f290cb7b0b38faba88f7ad4f9a004df63f
Nathan Liebrecht <nwliebrecht@gmail.com>
2016-08-07 14:11:52 -0600

    initial

---
+++ src/index.js
@@ -0,0 +1,32 @@
+"use strict";
+require("babel-core/register");
+require("babel-polyfill");
+
+const PouchDB: any = require('pouchdb-node');
+var db = new PouchDB('localdb');
+
+var doc = {
+  "_id": "mittens",
+  "name": "Mittens",
+  "occupation": "kitten",
+  "age": 3,
+  "hobbies": [
+    "playing with balls of yarn",
+    "chasing laser pointers",
+    "lookin' hella cute"
+  ]
+};
+
+
+(async function() {
+	try {
+		console.log("test");
+		await db.put(doc);
+
+		let doc = await db.get('mittens');
+		console.log(doc);
+	} catch(err) {
+		console.log("err", err);
+	}
+})();
+
