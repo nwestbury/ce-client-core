21d8adbd1c98e2d3d510b7ac5e87f7c0dddc1f05
Nathan Liebrecht <nwliebrecht@gmail.com>
2016-08-13 21:34:45 -0600

    stuff

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ce-client-core
 Shared repository for the Copy Everything client core for both desktop and mobile.
 
+## Setup build environment
+Read and execute https://github.com/nodejs/node-gyp#installation (used for leveldown) and run "npm install"
+
+
 ## Run
 npm run build && node ./build/index.js
--- src/index.js
+++ src/index.js
@@ -1,32 +1,207 @@
+/* Written by Nathan Liebrecht (c) */
+
 "use strict";
-require("babel-core/register");
 require("babel-polyfill");
 
-const PouchDB: any = require('pouchdb-node');
-var db = new PouchDB('localdb');
+import { Component, PropTypes, Children } from 'react';
+import { connect } from 'react-redux';
 
-var doc = {
-  "_id": "mittens",
-  "name": "Mittens",
-  "occupation": "kitten",
-  "age": 3,
-  "hobbies": [
-    "playing with balls of yarn",
-    "chasing laser pointers",
-    "lookin' hella cute"
-  ]
-};
+// Polyfill for pouchdb works for PouchDB 5.4.5, DO NOT UPGRADE!
+// Summary: pouch expects the following:
+// -an implementation of the standard openDatabase which we fudge (new between 5.3.1->5.4.5)
+// -the following polyfils: Buffer, atob, btoa, blob, process.nextTick
+// -a websql plugin
 
-(async function() {
-	try {
-		console.log("test");
-		let resp = await db.put(doc);
-		console.log(resp);
-		
-		let doc = await db.get('mittens');
-		console.log(doc);
-	} catch(err) {
-		console.log("err", err);
+global.Buffer = global.Buffer || require('buffer').Buffer;
+global.atob = global.atob || require('atob');
+global.btoa = global.btoa || require('btoa');
+require('blob-polyfill');
+window.openDatabase = window.openDatabase || true;
+window.sqlitePlugin = window.sqlitePlugin || require('react-native-sqlite-storage');
+process.nextTick = process.nextTick || setImmediate;
+
+const PouchDB = require('pouchdb-core');
+PouchDB.plugin(require('pouchdb-adapter-websql'))
+        .plugin(require('pouchdb-adapter-http'))
+        .plugin(require('pouchdb-replication'))
+		.plugin(require('pouchdb-upsert'));
+
+
+/////
+//redux actions
+
+function updateLastSeq(seq) {
+	return {
+		type: "UPDATE_LAST_SEQ",
+		seq: seq,
 	}
-})();
+}
+
+//adds or deletes copy
+function upsertCopy(copyObj) {
+	return {
+		type: 'UPSERT_COPY',
+		copyObj: copyObj,
+	}
+}
+
+/////
+//redux reducer
+
+function binarySearch(arr, docId) {
+  var low = 0, high = arr.length, mid;
+  while (low < high) {
+    mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
+    arr[mid]._id < docId ? low = mid + 1 : high = mid
+  }
+  return low;
+}
+
+const initialState = {
+	lastSeq: 0,
+	copies: [],
+}
+
+function copy(state = initialState, action) {
+	if(action.type === 'UPDATE_LAST_SEQ') {
+		return {...state, lastSeq: action.seq};
+	}
+	if(action.type === 'UPSERT_COPY') {
+		let copies = { ...state.copies };
+		const { copyObj } = action.copyObj;
+		
+		if(action.copyObj._deleted) {
+			var index = binarySearch(copies, id);
+			var copyObj = copies[index];
+			if (copyObj && copyObj._id === id) {
+				copies.splice(index, 1);
+			}
+		} else {
+			var index = binarySearch(copies, newDoc._id);
+			var copyObj = copies[index];
+			if (copyObj && copyObj._id === newDoc._id) { // update
+				copies[index] = newDoc;
+			} else { // insert
+				copies.splice(index, 0, newDoc);
+			}
+		}
+		return {...state, copies: copies };
+	}
+	return state;
+}
+
+module.exports.copyReducer = copy;
+
+/////
+//database actions
+
+function deleteClip(getDb, clipId) {
+	let db = getDb();
+	db.upsert(clipId, (doc) => ({...doc, _deleted: true}));
+}
+
+function addClip(getDb, clipObj) {
+	let db = getDb();
+	if(!clipObj._id.match(/clip\/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/))
+		throw "invalid id format";
+	
+	db.put(clipObj);
+}
+
+/////
+//database provider, use like the redux provider
+
+const DatabaseProvider = connect(select, actions)(class extends Component {
+	constructor() {
+		super();
+		
+		this.localdb = null;
+		this.remotedb = null;
+		this.changeListenner = null;
+		this.liveReplicator = null;
+		
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
+		this.getLocalDb = this.getLocalDb.bind(this);
+	}
+	getChildContext() {
+		let getDb = () => this.localdb;
+		
+		return {
+			databaseActions: {
+				deleteClip: deleteClip.bind(undefined, getDb),
+				addClip: addClip.bind(undefined, getDb),
+			}
+		};
+	}
+	getLocalDb() {
+		return this.localdb;
+	}
+	login(remotedbPath) {
+		this.localdb = new PouchDB('localdb');
+		this.remotedb = new PouchDB(remotedbPath);
+		
+		this.changeListenner = this.localdb.changes({live: true, since: this.props.lastSeq, include_docs: true}).on('change', function (change) {
+			let doc = change.doc;
+			if (doc.type === 'clip') {
+				this.props.upsertCopy(copyObj)
+			}
+		}).on('error', console.log.bind(console));
+		
+		this.liveReplicator = PouchDB.replicate(this.localdb, this.remotedb, {
+			live: true,
+			retry: true
+		}).on('change', function (info) {
+			console.log("// handle change");
+		}).on('paused', function (err) {
+			console.log("// replication paused (e.g. replication up to date, user went offline)");
+		}).on('active', function () {
+			console.log("// replicate resumed (e.g. new changes replicating, user went back online)");
+		}).on('denied', function (err) {
+			console.log("// a document failed to replicate (e.g. due to permissions)");
+		}).on('complete', function (info) {
+			console.log("// handle complete");
+		}).on('error', function (err) {
+			console.log("// handle error");
+		});
+	}
+	logout() {
+		this.changeListenner.cancel();
+		this.liveReplicator.cancel();
+		this.localdb.destroy();
+		this.remotedb.destroy();
+	}
+});
+
+function select(store) {
+	return {
+		lastSeq: store.copylist.lastSeq,
+	}
+}
+
+function actions(dispatch) {
+	return {
+		updateLastSeq: (lastSeq) => dispatch(updateLastSeq(lastSeq)),
+		upsertCopy: (copyObj) => dispatch()
+	}
+}
+
+/////
+//database connector HOC (equivalent to redux connect)
+
+class DatabaseConnect extends Component {
+	
+}
+
+module.exports = {
+	DatabaseProvider,
+	copyReducer,
+	copyActions: { updateLastSeq, upsertCopy },
+}
+
+
+
+
+
+
 
